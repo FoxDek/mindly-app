b3de5dad8c49ecf3d1ce81f2ea49c09ed80a113f
Tema <tema_foxes@mail.ru>
2026-01-11 14:26:25 +0300

    commit with convex type error

--- app/(tabs)/store.tsx
+++ app/(tabs)/store.tsx
@@ -1,8 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { useQuery } from 'convex/react'
+import { api } from '@/convex/_generated/api'
+import { get } from '../../convex/tasks';
 
 export default function Store() {
+  const tasks = useQuery(api.tasks.get)
+
   return (
     <SafeAreaView>
       <Text>store</Text>
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,32 +1,52 @@
 import { Stack } from "expo-router";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import SplashScreen from "./splash";
 import { useUserActions, useUserData } from "@/store/useUserStore";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+
+const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
+  unsavedChangesWarning: false,
+});
 
 export default function RootLayout() {
-  const userData = useUserData(); 
-  const { loadUserData } = useUserActions()
+  return (
+    <ConvexProvider client={convex}>
+      <AppContent />
+    </ConvexProvider>
+  );
+}
+
+function AppContent() {
+  const userData = useUserData();
+  const { loadUserData } = useUserActions();
 
   useEffect(() => {
     loadUserData();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   if (!userData) {
     return <SplashScreen />;
   }
-  
+
   return (
-  <Stack screenOptions={{headerShown: false}}>
-    <Stack.Screen name="(tabs)"/>
-    <Stack.Screen name="(modals)" options={{ 
-      headerShown: false,
-      presentation: 'transparentModal',
-      animation: 'fade',
-      animationDuration: 300
-    }} />
-    
-  </Stack>
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen name="(tabs)" />
+      <Stack.Screen
+        name="(modals)"
+        options={{
+          headerShown: false,
+          presentation: "transparentModal",
+          animation: "fade",
+          animationDuration: 300,
+        }}
+      />
+    </Stack>
   );
 }
+
 
